8675703af0dfb8c3472f90c861c74f9628a1dfbd
Mudit Shukla <visotecq@gmail.com>
2024-10-13 19:23:35 +0530

    listing feature

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Form,Container,Row,Col} from 'react-bootstrap';
+import {Form,Container,Row,Col,Card} from 'react-bootstrap';
 import AWS from 'aws-sdk';
-
 
 window.Buffer = window.Buffer || require("buffer").Buffer;
 function App() {
@@ -22,11 +21,47 @@ const myBucket = new AWS.S3({
   params: { Bucket: S3_BUCKET},
   region: REGION,
 })
+const myBucketList = new AWS.S3({
+  params: { Bucket: S3_BUCKET, Delimiter:'/'},
+  region: REGION,
+})
+
+
+  const [listFiles, setListFiles] = useState([]);
+
+  useEffect(() => {
+    myBucketList.listObjects((err, data) => {
+      if (err) {
+        console.log(err, err.stack);
+      } else {
+        setListFiles(data.Contents);
+        //  console.log(data.Contents);
+      }
+    });
+  }, []);
 
 
 
-
-
+  
+console.log(listFiles)
+const s3_url = 'https://uploadimagesfromuser.s3.amazonaws.com/';
+const pics = listFiles.map((item)=>
+<Col sm={true}>
+    <Card style={{ width: '18rem' }}>
+    <Card.Img variant="top" src={s3_url+item.Key} height={'200rem'}/>
+    <Card.Body>
+      <Card.Title>Pic Courtesy 
+      
+      </Card.Title>
+      <Card.Text>
+        Photographer Name
+      </Card.Text>
+     
+    </Card.Body>
+  </Card>
+  </Col>
+ 
+)
   const [progress , setProgress] = useState(0);
   const [selectedFile, setSelectedFile] = useState(null);
 
@@ -74,6 +109,12 @@ const myBucket = new AWS.S3({
         </Col>
         <Col sm={true}>
         </Col>
+      </Row>
+    </Container>
+
+    <Container style={{ marginTop:'80px' }}>
+      <Row>
+        {pics}
       </Row>
     </Container>
    
