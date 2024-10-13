90dd4af2454ac51333a20f568a037be2bee70cc9
Mudit Shukla <visotecq@gmail.com>
2024-10-13 14:53:10 +0530

    updates aws client

--- src/App.js
+++ src/App.js
@@ -2,48 +2,52 @@ import React,{useState} from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Form from 'react-bootstrap/Form';
-import S3 from 'react-aws-s3';
+import AWS from 'aws-sdk'
+
+
 window.Buffer = window.Buffer || require("buffer").Buffer;
 function App() {
- 
-const config = {
-    bucketName: 'upluploadimagesfromuser',
-    dirName: 'nov-24', /* optional */
-    region: 'us-east-1',
-}
+  const S3_BUCKET ='uploadimagesfromuser';
+  const REGION ='us-east-1';
+  
 
-const ReactS3Client = new S3(config);
-/*  Notice that if you don't provide a dirName, the file will be automatically uploaded to the root of your bucket */
+  AWS.config.update({
+    accessKeyId: 'AKIAU67XKWU4M6HRWSCL',
+    secretAccessKey: '3MpfRn/cYpFsOQ96A9mZEXghJrNCugN1jlsLnQ/d'
+})
 
-/* This is optional */
-const newFileName = 'test-file';
+const myBucket = new AWS.S3({
+  params: { Bucket: S3_BUCKET},
+  region: REGION,
+})
 
-const [file, setFile] = useState()
-function handleUpload(e) {
-  setFile(e.target.files[0])
 
-    }
 
-function handleSubmit(e){
-    e.preventDefault();
-    console.log(file.name)
-    const name = file.name
-    ReactS3Client
-    .uploadFile(file, name)
-    .then(data => console.log(data))
-    .catch(err => console.error(err))
-}
-    
- 
-  /**
-   * {
-   *   Response: {
-   *     bucket: "myBucket",
-   *     key: "image/test-image.jpg",
-   *     location: "https://myBucket.s3.amazonaws.com/media/test-file.jpg"
-   *   }
-   * }
-   */
+  const [progress , setProgress] = useState(0);
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleUpload = (e) => {
+      setSelectedFile(e.target.files[0]);
+  }
+
+  const handleSubmit = (e) => {
+      e.preventDefault();
+      const params = {
+          Body: selectedFile,
+          Bucket: S3_BUCKET,
+          Key: selectedFile.name
+      };
+
+      myBucket.putObject(params)
+          .on('httpUploadProgress', (evt) => {
+              setProgress(Math.round((evt.loaded / evt.total) * 100))
+          })
+          .send((err) => {
+              if (err) console.log(err)
+          })
+  }
+
+
 
 
 
@@ -53,7 +57,7 @@ function handleSubmit(e){
     <>
     <Form onSubmit={handleSubmit}>
       <Form.Group controlId="formFile" className="mb-3">
-        <Form.Label>Default file input example</Form.Label>
+        <Form.Label>Default file input example {progress}%</Form.Label>
         <Form.Control type="file" onChange={handleUpload} />
       </Form.Group>
         <Form.Control type='submit' name='Submit'/>
